a7b63a1f3d12e7ea2ebd340767aa19b924bb548d
Tien Pham <tien@phaw.io>
2020-08-05 15:54:42 +0700

    Add sliders to change `cap height` and `line gap`

--- example/App.tsx
+++ example/App.tsx
@@ -3,6 +3,7 @@ import { StyleSheet, View, Text } from 'react-native'
 import capsize from 'react-native-capsize'
 import { useFonts, Inter_400Regular } from '@expo-google-fonts/inter'
 import { AppLoading } from 'expo'
+import Slider from '@react-native-community/slider'
 
 const fontMetrics = {
   capHeight: 2048,
@@ -17,15 +18,48 @@ export default function App() {
     Inter_400Regular,
   })
 
+  const [capHeight, setCapHeight] = React.useState(16)
+  const [lineGap, setLineGap] = React.useState(4)
+
+  const textStyle = {
+    ...capsize({
+      capHeight,
+      lineGap,
+      fontMetrics,
+    }),
+  }
+
   return fontsLoaded ? (
     <View style={styles.container}>
-      <Text style={styles.text}>
-        Lorem ipsum Lolor sit amet, Lonsectetur adipiscing elit. Duis eu ornare
-        nisi, sed feugiat metus. Pellentesque rutrum vel metus non dignissim.
-        Aenean egestas neque mattis mi maximus luctus. Praesent et commodo dui,
-        nec eleifend lectus. Pellentesque blandit nisi tellus, id efficitur urna
-        consectetur id. Sed convallis tempor dui vel aliquet.
-      </Text>
+      <Text>{`Cap Height: ${capHeight}`}</Text>
+      <Slider
+        minimumValue={8}
+        maximumValue={28}
+        onValueChange={setCapHeight}
+        onSlidingComplete={setCapHeight}
+        step={1}
+        minimumTrackTintColor="rgb(237, 100, 166)"
+        maximumTrackTintColor="#FDE2EB"
+      />
+      <Text>{`Line Gap: ${lineGap}`}</Text>
+      <Slider
+        minimumValue={4}
+        maximumValue={28}
+        onValueChange={setLineGap}
+        onSlidingComplete={setLineGap}
+        step={1}
+        minimumTrackTintColor="rgb(237, 100, 166)"
+        maximumTrackTintColor="#FDE2EB"
+      />
+      <View style={styles.textContainer}>
+        <Text style={textStyle}>
+          Lorem ipsum Lolor sit amet, Lonsectetur adipiscing elit. Duis eu
+          ornare nisi, sed feugiat metus. Pellentesque rutrum vel metus non
+          dignissim. Aenean egestas neque mattis mi maximus luctus. Praesent et
+          commodo dui, nec eleifend lectus. Pellentesque blandit nisi tellus, id
+          efficitur urna consectetur id. Sed convallis tempor dui vel aliquet.
+        </Text>
+      </View>
     </View>
   ) : (
     <AppLoading />
@@ -38,11 +72,8 @@ const styles = StyleSheet.create({
     paddingVertical: 28,
     paddingHorizontal: 16,
   },
-  text: {
-    ...capsize({
-      fontMetrics,
-      capHeight: 18,
-      lineGap: 8,
-    }),
+  textContainer: {
+    backgroundColor: '#FDE2EB',
+    marginTop: 16,
   },
 })
